397fdfcc61f4facc8fa6783ba442ecd2ee21fb8c
Furkan Tunç <furkant88@gmail.com>
2025-07-27 15:22:55 +0300

    Rol altyapısı kuruldu

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -6,13 +6,14 @@ namespace Domain.Entities;
 
 public class User : Entity<User, long>
 {
-    public string Email { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public StatusType Status { get; set; }
-    public string PasswordHash { get; set; } = null!;
-    public DateTime? LastPasswordChangeDate { get; set; }
-    public int LoginTryCount { get; set; }
-    public bool IsBlocked { get; set; }
-    public ICollection<UserRole> Roles { get; set; }
+    public string Email { get; protected set; }
+    public string FirstName { get; protected set; }
+    public string LastName { get; protected set; }
+    public StatusType Status { get; protected set; }
+    public string PasswordHash { get; protected set; } = null!;
+    public DateTime? LastPasswordChangeDate { get; protected set; }
+    public int LoginTryCount { get; protected set; }
+    public bool IsBlocked { get; protected set; }
+
+    public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
 }
