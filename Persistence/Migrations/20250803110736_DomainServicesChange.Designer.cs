01a4eab010692f7bb1a64eaece8371bc29c1e29b
Furkan Tunç <furkant88@gmail.com>
2025-08-03 14:09:01 +0300

    DI ve migration eklendi.

--- Persistence/Migrations/20250803110736_DomainServicesChange.Designer.cs
+++ Persistence/Migrations/20250803110736_DomainServicesChange.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Contracts;
 
@@ -11,9 +12,11 @@ using Persistence.Contracts;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250803110736_DomainServicesChange")]
+    partial class DomainServicesChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence/Migrations/20250803110736_DomainServicesChange.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DomainServicesChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_Role_RoleId",
+                table: "UserRole");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRole_User_UserId",
+                table: "UserRole");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRole",
+                table: "UserRole");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Role",
+                table: "Role");
+
+            migrationBuilder.RenameTable(
+                name: "UserRole",
+                newName: "UserRoles");
+
+            migrationBuilder.RenameTable(
+                name: "Role",
+                newName: "Roles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRole_UserId",
+                table: "UserRoles",
+                newName: "IX_UserRoles_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRole_RoleId",
+                table: "UserRoles",
+                newName: "IX_UserRoles_RoleId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Roles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Roles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<short>(
+                name: "Status",
+                table: "Roles",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "PermissionGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PermissionGroupId = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Permissions_PermissionGroups_PermissionGroupId",
+                        column: x => x.PermissionGroupId,
+                        principalTable: "PermissionGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RolePermissions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<long>(type: "bigint", nullable: false),
+                    PermissionId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RolePermissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RolePermissions_Permissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "Permissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RolePermissions_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Permissions_PermissionGroupId",
+                table: "Permissions",
+                column: "PermissionGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolePermissions_PermissionId",
+                table: "RolePermissions",
+                column: "PermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RolePermissions_RoleId",
+                table: "RolePermissions",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_User_UserId",
+                table: "UserRoles",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_User_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropTable(
+                name: "RolePermissions");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "PermissionGroups");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Roles");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                newName: "UserRole");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                newName: "Role");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRoles_UserId",
+                table: "UserRole",
+                newName: "IX_UserRole_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRoles_RoleId",
+                table: "UserRole",
+                newName: "IX_UserRole_RoleId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRole",
+                table: "UserRole",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Role",
+                table: "Role",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_Role_RoleId",
+                table: "UserRole",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRole_User_UserId",
+                table: "UserRole",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
